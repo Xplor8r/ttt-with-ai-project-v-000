154737f8f91d6a20c40ba0af5c2ebe4aefde0c65
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-15 22:59:07 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,12 +6,8 @@ module Players
         choice = corners.sample
       elsif board.turn_count == 1
         choice = "5"
-      else
-        Game::WIN_COMBINATIONS.find do |cmb|
-          if cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
-            choice = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
-          end
-        end
+      elsif 
+        
       end
       until  board.valid_move?(choice)
         choice = (1..9).to_a.sample.to_s
